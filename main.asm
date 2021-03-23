abf9587d790aad8313a9c93e8817cffd6d71073b
Jody <jers_033@hotmail.com>
2021-03-23 10:04:47 -0600

    Creando html

--- main.asm
+++ main.asm
@@ -1,4 +1,4 @@
-include proced.asm
+;include proced.asm
 
 .model small
 .stack 100h
@@ -28,12 +28,12 @@ menu  dw menu0,separacion,menu1,menu2,menu3,menu4,menu5,separacion
 
 ;Cadenas de ingreso en el menú
 ingreso db 0ah,0dh, 'Ingrese una opcion para continuar','$'
-nUno db 0ah,0dh, 'Se ha seleccionado el numero 1','$'
+nUno db 0ah,0dh, 'Ingrese la ruta del archivo a leer','$'
 nDos db 0ah,0dh, 'Se ha seleccionado el numero 2','$'
 nTres db 0ah,0dh, 'Se ha seleccionado el numero 3','$'
 nCuatro db 0ah,0dh, 'Se ha seleccionado el numero 4','$'
-nCinco db 0ah,0dh, 'Se ha seleccionado el numero 5','$'
-
+nCinco db 0ah,0dh, 'Enter para salir del programa','$'
+nNoExiste db 0ah, 0dh, 'Opcion no disponible, seleccione otra','$'
 ;ruta del archivo temporal y su manejador
 ruta db "texto.txt",0  
 manejador dw 0
@@ -44,13 +44,13 @@ msjError db 0ah,0dh, 'Hubo un error en la lectura del archivo.', '$'
 archivoLeido db 0ah,0dh, 'Archivo leido correctamente.', '$'
 rutaPorTeclado dw 30 dup("$")
 lectura db 60 dup("$")
-instruccion db 60 dup("$")
+instruccion db 8000 dup("$")
 letra db ?,"$"
 saltoLinea db 0ah, 0dh, "$"
 escribir db ';','$'
 contadorOp db 0
 contadorNum dw 0
-numeros dw 60 dup(0)
+numeros dw 600 dup(0)
 
 ;comparadores de instrucciones
 
@@ -69,6 +69,24 @@ val_c db '/valor', '$'
 padre_a db 'operaciones','$'
 padre_c db '/operaciones','$'
 
+;elementos de hora y fecha
+
+hora db 10 dup("$") ;hora::minuto:segundo
+
+
+
+;elementos html
+htmlInicio dw '<html><head><title>resultados</title></head>','$'
+htmlBody dw '<body><p>','$'
+htmlCierre dw '</p></body></html>','$'
+
+;elementos para crear archivo
+nombreSalida db "res.html",0   
+manejadorSalida dw ?
+errorCreacion db 'Error al crear el archivo','$'
+
+
+
 ;db -> dato byte -> 8 bits
 ;dw -> dato word -> 16 bits
 ;dd -> doble word -> 32 bits
@@ -80,72 +98,98 @@ padre_c db '/operaciones','$'
         MOV AX, @data
         MOV DS, AX
         ;=================================================================
+        imprimirTextos inicio,9
+        enter
 
-        pedirRutaPorTeclado rutaPorTeclado ;Pedir la ruta por teclado
-	    abrirArchivo rutaPorTeclado,manejador ;Abrir archivo
-        jc errorApertura ; Verificar si existe un error
-        escribirFin manejador, escribir ;Escribir el EOF al archivo de entrada
-		cerrarArchivo manejador ;Cerrar el archivo
+        menuPrincipal:
+            cls
+            imprimirTextos menu,8
+            imprimirArchivo saltoLinea
+            mov ah,9
+            lea dx,ingreso ;Impresión de cadena para ingreso de una opción del menú
+            int 21h
+            leer:
+                mov ah, 1   ; Leer entrada del teclado
+                int 16h
+                jz  leer    ; Determinar si se ha presionado algo
+                mov ah, 0   ; Obtener tecla presionada
+                int 16h    
+                cmp al, '1' ; Comparar la tecla presionada
+                je numeroUno  
+                cmp al, '2' ; Comparar la tecla presionada
+                je numeroDos
+                cmp al, '3' ; Comparar la tecla presionada
+                je numeroTres
+                cmp al, '4' ; Comparar la tecla presionada
+                je numeroCuatro
+                cmp al, '5' ; Comparar la tecla presionada
+                je numeroCinco
+                jne noDisponible
 
-	    abrirArchivo rutaPorTeclado,manejador ;Abrir archivo
-        jc errorApertura ; Verificar si existe un error
-        leerArchivo lectura, manejador, letra, instruccion, contadorOp, sum_a,sum_c,res_a,res_c,mul_a,mul_c,div_a,div_c,val_a,val_c,op_a,op_c, numeros, contadorNum,saltoLinea,padre_a,padre_c
-		cerrarArchivo manejador ;Cerrar el archivo
-        imprimirArchivo lectura
-        jmp fin
+            numeroUno: ;Si se seleccionó la opción 1
+                mov ah,9
+                lea dx, nUno
+                int 21h
+                imprimirArchivo saltoLinea
+                pedirRutaPorTeclado rutaPorTeclado;Pedir la ruta por teclado
+                abrirArchivo rutaPorTeclado,manejador ;Abrir archivo
+                jc errorApertura ; Verificar si existe un error
+                escribirFin manejador, escribir ;Escribir el EOF al archivo de entrada
+                cerrarArchivo manejador ;Cerrar el archivo
 
-        errorApertura: ;Imprimir el error y terminar
-            imprimirArchivo msjError ;Imprimir msj de error
-            cerrarArchivo manejador  ;Cerrar el archivo
-            jmp fin ;Saltar al final del programa
+                abrirArchivo rutaPorTeclado,manejador ;Abrir archivo
+                jc errorApertura ; Verificar si existe un error
+                leerArchivo lectura, manejador, letra, instruccion, contadorOp, sum_a,sum_c,res_a,res_c,mul_a,mul_c,div_a,div_c,val_a,val_c,op_a,op_c, numeros, contadorNum,saltoLinea,padre_a,padre_c
+                cerrarArchivo manejador ;Cerrar el archivo
+                ;imprimirArchivo lectura
+                crearHtml nombreSalida,manejadorSalida,errorCreacion
+                writeHtml nombre,manejadorSalida,errorCreacion,htmlInicio 
+                writeHtml nombre,manejadorSalida,errorCreacion,htmlBody
+                writeHtml nombre,manejadorSalida,errorCreacion,instruccion
+                writeHtml nombre,manejadorSalida,errorCreacion,htmlCierre
+                cerrarArchivo manejadorSalida
+                enter
+                jmp menuPrincipal
+
+                errorApertura: ;Imprimir el error y terminar
+                    imprimirArchivo msjError ;Imprimir msj de error
+                    enter
+                    ;cerrarArchivo manejador  ;Cerrar el archivo
+                    jmp menuPrincipal ;Saltar al final del programa
+
+                jmp menuPrincipal
+            numeroDos: ;Si se seleccionó la opción 2
+                mov ah,9
+                lea dx, nDos
+                int 21h
+                enter
+                jmp menuPrincipal
+            numeroTres: ;Si se seleccionó la opción 3
+                mov ah,9
+                lea dx, nTres
+                int 21h
+                enter
+                jmp menuPrincipal
+            numeroCuatro: ;Si se seleccionó la opción 4
+                mov ah,9
+                lea dx, nCuatro
+                int 21h
+                enter
+                jmp menuPrincipal
+            numeroCinco: ;Si se seleccionó la opción 5, entonces es salida
+                mov ah,9
+                lea dx, nCinco
+                int 21h
+                enter
+                jmp fin
+            ;imprimirTextos inicio,9
+            noDisponible:
+                imprimirArchivo nNoExiste
+                imprimirArchivo saltoLinea
+                enter
+                jmp leer
 
 
-        imprimirTextos menu,8
-        mov ah,9
-        lea dx,ingreso ;Impresión de cadena para ingreso de una opción del menú
-        int 21h
-        leer:
-            mov ah, 1   ; Leer entrada del teclado
-            int 16h
-            jz  leer    ; Determinar si se ha presionado algo
-            mov ah, 0   ; Obtener tecla presionada
-            int 16h    
-            cmp al, '1' ; Comparar la tecla presionada
-            je numeroUno  
-            cmp al, '2' ; Comparar la tecla presionada
-            je numeroDos
-            cmp al, '3' ; Comparar la tecla presionada
-            je numeroTres
-            cmp al, '4' ; Comparar la tecla presionada
-            je numeroCuatro
-            cmp al, '5' ; Comparar la tecla presionada
-            jne leer
-        numeroUno: ;Si se seleccionó la opción 1
-            mov ah,9
-            lea dx, nUno
-            int 21h
-            jmp fin
-        numeroDos: ;Si se seleccionó la opción 2
-            mov ah,9
-            lea dx, nDos
-            int 21h
-            jmp fin
-        numeroTres: ;Si se seleccionó la opción 3
-            mov ah,9
-            lea dx, nTres
-            int 21h
-            jmp fin
-        numeroCuatro: ;Si se seleccionó la opción 4
-            mov ah,9
-            lea dx, nCuatro
-            int 21h
-            jmp fin
-        numeroCinco: ;Si se seleccionó la opción 5, entonces es salida
-            mov ah,9
-            lea dx, nCinco
-            int 21h
-            jmp fin
-        ;imprimirTextos inicio,9
         fin:
             
         .exit
--- proced.asm
+++ proced.asm
@@ -55,6 +55,7 @@ endm
 
 ;======================PEDIR RUTA POR TECLADO===========================
 pedirRutaPorTeclado macro rutaPorTeclado
+    local pedir, fin_pedir
     lea si, rutaPorTeclado
     pedir:
         mov ah, 01H
@@ -72,13 +73,13 @@ endm
 
 ;======================LEER ARCHIVO===========================
 leerArchivo macro lectura, manejador, letra, instruccion, contadorOp, sum_a,sum_c,res_a,res_c,mul_a,mul_c,div_a,div_c,val_a,val_c,op_a,op_c, numeros, contadorNum,saltoLinea,padre_a,padre_c
-    local ciclo, finCiclo
+    local ciclo, finCiclo, getId, getNumero, verificarNumero, verificarId, escribirReporte
     mov si, offset instruccion
     mov di, offset lectura
     ciclo:
         ;Conseguir caracter de entrada, 1 x 1
-        imprimirArchivo lectura
-        imprimirArchivo saltoLinea
+        ;imprimirArchivo lectura
+        ;imprimirArchivo saltoLinea
         mov ah, 3Fh
         mov bx, manejador
         mov cx, 1
@@ -96,6 +97,9 @@ leerArchivo macro lectura, manejador, letra, instruccion, contadorOp, sum_a,sum_
         ;Verificar si se cerró una operación y ver si hay un nuevo id para guardar
         cmp letra, '>'
         je verificarId
+
+        cmp letra, '-'
+        je getNumero
 
         cmp letra, 0dh
         je ciclo
@@ -127,6 +131,15 @@ leerArchivo macro lectura, manejador, letra, instruccion, contadorOp, sum_a,sum_
             inc di
             jmp ciclo
             esNegativo:
+                mov cl, '('
+                mov [si],cl
+                inc si
+                mov cl, letra
+                mov [si],cl
+                inc si
+                mov cl, ')'
+                mov [si], cl
+                inc si
                 mov ch,1
                 mov cl,2
                 sub ch,cl
@@ -153,7 +166,7 @@ endm
 ;======================Convertir a número===========================
 
 convertirNumeros macro lectura, numeros, contadorNum
-    local ciclo,finalizar,negativo, fin
+    local ciclo,finalizar,negativo, fin, omitirH
     push si
 
     mov si, offset lectura
@@ -175,48 +188,53 @@ convertirNumeros macro lectura, numeros, contadorNum
     finalizar:  
         limpiarLector lectura
         mov di, offset lectura
-            
+        mov cx, ax    
         aam 
         add ah, 30h
         add al, 30h   
-        pop si 
-        popf
-        js negativo 
+        pop si  
+        cmp ah,30h
+        je omitirH            
         mov [si],ah
         inc si
+        omitirH:
         mov [si],al
-        inc si   
+        inc si 
+        popf
+        js negativo  
         ;push ax  
         push di
         mov di,contadorNum
-        sub ah, 30h
-        sub al, 30h
+        mov ax, cx
+        ;sub ah, 30h
+        ;sub al, 30h
         mov numeros[di],ax
         inc contadorNum
         inc contadorNum
         pop di
         jmp fin 
         negativo:
+        mov ax, cx
+        ;sub ah, 30h
+        ;sub al, 30h
         mov bx,-1
         mul bx
         ;push ax
         push di
         mov di,contadorNum
-        sub ah, 30h
-        sub al, 30h
         mov numeros[di],ax
         inc contadorNum
         inc contadorNum
         pop di
-        mov cl,'-'
-        add ah, 30h
-        add al, 30h
-        mov [si],cl
-        inc si
-        mov [si],ah
-        inc si
-        mov [si],al
-        inc si                      
+        ;mov cl,'-'
+        ;add ah, 30h
+        ;add al, 30h
+        ;mov [si],cl
+        ;inc si
+        ;mov [si],ah
+        ;inc si
+        ;mov [si],al
+        ;inc si                      
         fin: 
         
 endm
@@ -242,6 +260,7 @@ endm
 ejecutarOp macro lectura, sum_a,sum_c,res_a,res_c,mul_a,mul_c,div_a,div_c,val_a,val_c,op_a,op_c, contadorOp, numeros, contadorNum,padre_a,padre_c
     ;push ax
     ;push cx
+    local agregarDiv, agregarMulti, agregarOp, agregarResta, agregarSuma, exeDiv, exeMulti, exeResta, exeSuma, salir,finOperacion, esNegativo, divNegativo,continuarDiv, mulNegativo, continuarMul,resNegativo, res2Negativo, continuarRes, sumNegativo, sum2Negativo, continuarSum, esMayor, divNegativo, div2Negativo, div2tNegativo, mulNegativo, mul2Negativo, mul2tNegativo
     comparar lectura, sum_a
     je agregarSuma
     comparar lectura, res_a
@@ -308,13 +327,34 @@ ejecutarOp macro lectura, sum_a,sum_c,res_a,res_c,mul_a,mul_c,div_a,div_c,val_a,
         dec contadorNum
         mov si,contadorNum
         mov ax, numeros[si]
+
+        test ax,ax
+        js sumNegativo
         add ax,bx
-        ;push ax
-        mov numeros[si],ax
-        inc contadorNum
-        inc contadorNum
-        pop si
-        jmp salir
+        jmp continuarSum
+
+        sumNegativo:
+            neg ax
+            test bx,bx
+            js sum2Negativo
+            sub ax,bx
+            neg ax
+            jmp continuarSum
+
+            sum2Negativo:
+                sub ax,bx
+                neg ax
+                jmp continuarSum
+
+        continuarSum:
+            ;push ax
+            mov numeros[si],ax
+            pop si
+            ;test ax,ax
+            ;pushf
+            inc contadorNum
+            inc contadorNum      
+            jmp salir
     exeResta:
         ;pop ax
         push si
@@ -328,14 +368,38 @@ ejecutarOp macro lectura, sum_a,sum_c,res_a,res_c,mul_a,mul_c,div_a,div_c,val_a,
         dec contadorNum
         mov si,contadorNum
         mov ax, numeros[si]
+
+        test ax,ax
+        js resNegativo:
         sub ax,bx
+        jmp continuarRes
         ;push ax
-        mov numeros[si],ax
-        inc contadorNum
-        inc contadorNum
-        pop si
-        jmp salir
+
+        resNegativo:
+            neg ax
+            test bx,bx
+            js res2Negativo
+            add ax,bx
+            neg ax
+            jmp continuarRes
+
+            res2Negativo:
+                ;neg bx
+                add ax,bx
+                neg ax
+                jmp continuarRes
+
+        
+        continuarRes:
+            mov numeros[si],ax
+            pop si
+            ;test ax,ax
+            ;pushf
+            inc contadorNum
+            inc contadorNum
+            jmp salir
     exeMulti:
+        
         ;pop ax
         push si
         dec contadorNum
@@ -348,13 +412,39 @@ ejecutarOp macro lectura, sum_a,sum_c,res_a,res_c,mul_a,mul_c,div_a,div_c,val_a,
         dec contadorNum
         mov si,contadorNum
         mov ax, numeros[si]
-        imul bx
+        xor dx,dx
+        test ax,ax
+        js mulNegativo
+        test bx,bx
+        js mul2Negativo
+        mul bx
+        jmp continuarMul
+        mulNegativo:
+            neg ax
+            test bx,bx
+            js mul2tNegativo
+            mul bx
+            neg ax
+            jmp continuarMul   
+        mul2tNegativo:
+            neg bx
+            mul bx 
+            jmp continuarMul              
         ;push ax
-        mov numeros[si],ax
-        inc contadorNum
-        inc contadorNum
-        pop si
-        jmp salir
+        mul2Negativo:
+            neg bx
+            mul bx
+            neg ax
+            jmp continuarMul
+        continuarMul:
+            mov numeros[si],ax
+            pop si
+            ;test ax,ax
+            ;pushf
+            inc contadorNum
+            inc contadorNum
+            jmp salir
+
     exeDiv:
         ;pop ax
         push si
@@ -369,43 +459,171 @@ ejecutarOp macro lectura, sum_a,sum_c,res_a,res_c,mul_a,mul_c,div_a,div_c,val_a,
         mov si,contadorNum
         mov ax, numeros[si]
         xor dx,dx
-        idiv bx
+        test ax,ax
+        js divNegativo
+        test bx,bx
+        js div2Negativo
+        div bx
+        jmp continuarDiv
+        divNegativo:
+            neg ax
+            test bx,bx
+            js div2tNegativo
+            xor dx,dx
+            div bx
+            neg ax
+            jmp continuarDiv   
+        div2tNegativo:
+            neg bx
+            xor dx,dx
+            div bx 
+            jmp continuarDiv               
         ;push ax
-        mov numeros[si],ax
-        inc contadorNum
-        inc contadorNum
-        pop si
-        jmp salir
+        div2Negativo:
+            neg bx
+            xor dx,dx
+            div bx
+            neg ax
+            jmp continuarDiv
+        continuarDiv:
+            mov numeros[si],ax
+            pop si
+            ;test ax,ax
+            ;pushf
+            inc contadorNum
+            inc contadorNum
+            jmp salir
 
     agregarOp:
         addOp lectura
         jmp salir
     finOperacion:
+        push di
         dec contadorNum
         dec contadorNum
-        mov si,contadorNum
-        mov ax, numeros[si]
-        add al, 30h
-        add ah, 30h
-        mov [di],' '
-        inc di
-        mov [di],'='
-        inc di
-        mov [di],' '
-        inc di
-        mov [di],ax
-        inc di
-        inc di
-        mov [di], 0dh
-        inc di
-        inc contadorOp
+        mov di,contadorNum
+        mov ax, numeros[di]
+        ;cmp ax,0
+        ;popf
+        ;jg noEsNegativo
+        ;js esNegativo
+        test ax,ax
+        js esNegativo
+        noEsNegativo:
+        ;add ah, 30h
+        ;add al, 30h
+        mov [si],' '
+        inc si
+        mov [si],'='
+        inc si
+        mov [si],' '
+        inc si
+        ;mov [si],ax
+        ;aam
+        ;add ah,30h
+        ;add al,30h
+        ;mov [si],ah
+        ;inc si
+        ;mov [si],al
+        ;inc si
+        numberToAscii
+        mov [si], '<'
+        inc si
+        mov [si], '/'
+        inc si
+        mov [si], 'b'
+        inc si
+        mov [si], 'r'
+        inc si
+        mov [si], '>'
+        inc si
+        jmp salir
+        esNegativo:
+            push bx
+            mov bx,-1
+            mul bx
+            ;add ah, 30h
+            ;add al, 30h
+            mov [si],' '
+            inc si
+            mov [si],'='
+            inc si
+            mov [si],' '
+            inc si
+            mov [si],'-'
+            inc si
+            ;mov [si],ax
+            ;aam
+            ;add ah,30h
+            ;add al,30h
+            ;mov [si],ah
+            ;inc si
+            ;mov [si],al
+            ;inc si
+            numberToAscii
+            mov [si], '<'
+            inc si
+            mov [si], '/'
+            inc si
+            mov [si], 'b'
+            inc si
+            mov [si], 'r'
+            inc si
+            mov [si], '>'
+            inc si
+            pop bx
         ;realizar operacion
-    salir:    
+    salir: 
+        inc contadorOp
+        pop di   
         limpiarLector lectura
         mov di, offset lectura
         ;pop cx
         ;pop ax
 endm
+;=================================================================
+
+
+;======================Numeros grandes===========================
+
+numberToAscii macro
+    local esMenor, digito, recorrerPila, unDigito, ciclo, esIgual
+        xor dx,dx
+        mov cx, 1
+        mov bx, 10
+    digito:
+        cmp ax,10
+        jb esMenor
+        div bx
+        inc cx
+        push dx
+        jmp digito
+    esMenor:
+        push ax
+        cmp cx,1
+        je unDigito
+        ciclo:
+            cmp cx,1
+            je unDigito
+            dec cx
+            pop dx
+            add dl, 30h
+            mov [si],dl
+            inc si
+            jmp ciclo
+    unDigito:
+        pop dx
+        mov ax,dx
+        ;add ah, 30h
+        ;mov [si],ah
+        ;inc si
+        add al, 30h
+        mov [si],al
+        inc si 
+endm
+
+
+
 ;=================================================================
 
 
@@ -520,4 +738,188 @@ endm
 ;=================================================================
 
 
+;======================LIMPIAR CONSOLA===========================
+cls macro
+    push ax
+    ;mov ah, 06h
+    ;mov al, 0
+    ;int 10h
+    mov ah,00
+    mov al,02
+    int 10h
+    pop ax
+endm
+;=================================================================
 
+
+;======================LIMPIAR CONSOLA===========================
+enter macro
+    local pedir, fin_pedir
+    push ax
+    push si
+    pedir:
+        mov ah, 01H
+        int 21h
+        cmp al, 0Dh
+        je fin_pedir
+        mov [si], al
+        inc si
+        jmp pedir
+    fin_pedir:   
+    pop si
+    pop ax    
+endm
+
+
+;=================================================================
+
+
+
+;======================CONSEGUIR HORA===========================
+
+getHora macro hora
+    mov si,offset hora
+    mov ah, 2ch
+    int 21h
+    ;Aqui se consigue la hora y los registros quedan así:
+    ;Ch = hora
+    ;Cl = minuto
+    ;Dh = segundo
+    ;Mando un número para decirle que es el último
+    xor ax,ax
+    mov al,ch
+    convertirHora 0
+    mov al,cl
+    convertirHora 0
+    mov al, dh
+    convertirHora 1
+
+endm
+
+
+;=================================================================
+
+
+;======================CONVERTIR HORA===========================
+
+convertirHora macro ultimo
+    local esCero,verificarUltimo, menor,agregarPuntos,fin
+    push cx
+    push dx
+    cmp al,0
+    je esCero
+    cmp al,10
+    jb menor
+    xor dx,dx
+    mov bx,10
+    div bx
+    aam
+    add al,30h 
+    mov [si],al
+    inc si
+    mov al,dl
+    aam
+    add al,30h
+    mov [si],al
+    inc si
+    jmp verificarUltimo
+
+    menor:
+        mov [si],30h
+        inc si    
+        aam
+        add al, 30h    
+        mov [si], al
+        inc si
+        jmp verificarUltimo
+    esCero:
+        mov [si],30h
+        inc si
+        mov [si],30h
+        inc si
+
+    verificarUltimo:
+        mov al, ultimo
+        cmp al,1
+        je fin
+        jne agregarPuntos
+
+    agregarPuntos:
+        mov [si],':'
+        inc si
+    fin:
+        pop dx
+        pop cx
+endm
+;=================================================================
+
+
+
+;======================CONSEGUIR FECHA===========================
+
+;=================================================================
+
+
+
+
+;======================CONVERTIR FECHA===========================
+
+;=================================================================
+
+
+
+
+;======================CREAR HTML===========================
+
+crearHtml macro nombre,manejador,errorCreacion
+    local error, fin
+    lea dx, nombre
+    mov cx,0
+    ;Crear el archivo
+    mov ah, 3ch
+    int 21h
+    mov manejador,ax
+    jc error
+    ;Cerrar el archivo
+    ;mov ah, 3Eh
+    ;mov bx, manejador
+    ;int 21h
+    jmp fin
+    error:
+        imprimirArchivo errorCreacion
+        enter
+        ;Rutina para terminar programa
+        ;mov ax, 4c00h
+        ;int 21h
+    fin:
+endm
+
+;=================================================================
+
+
+
+;======================ESCRIBIR HTML===========================
+writeHtml macro nombre,manejador,errorCreacion,contenido
+    mov ah,40h
+    mov bx,manejador
+    getArraySize contenido
+    lea dx,contenido
+    int 21h    
+    ;cerrarArchivo manejador
+endm
+;=================================================================
+
+;======================GET SIZE ARRAY===========================
+getArraySize macro array
+    local ciclo,fin
+    mov cx,0
+    lea si,array
+    ciclo:
+        cmp [si],'$'
+        je fin
+        inc si
+        inc cx
+        jmp ciclo
+    fin:
+endm
+;=================================================================
